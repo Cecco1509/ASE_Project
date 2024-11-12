53a69e67eb9674cd133fdc2e888f5a300682afd0
JanaStevanovic <janastevanovic68@gmail.com>
2024-11-12 20:51:47 +0100

    payments admin added config

--- PaymentsAdmin/app.py
+++ PaymentsAdmin/app.py
@@ -3,19 +3,22 @@ import requests, time
 from flask import Flask, request, make_response 
 from requests.exceptions import ConnectionError, HTTPError
 from werkzeug.exceptions import NotFound
+from python_json_config import ConfigBuilder
+
+builder = ConfigBuilder()
+config = builder.parse_config('../config.json')
 
 app = Flask(__name__, instance_relative_config=True) #instance_relative_config=True ? 
 
 def create_app():
     return app
 
-DATABASE_MANAGER_URL = 'http://dbmanager:5000'
 
 @app.route('/api/admin/currency/<int:user_id>', methods=['GET'])
 def get_transaction_history(user_id):
     try:
         # Prepare the request URL for fetching transaction history
-        transaction_history_url = DATABASE_MANAGER_URL+f'/currencytransaction/{user_id}'
+        transaction_history_url = config.urls.db_manager+f'/currencytransaction/{user_id}'
         
 
         # Make a GET request to the database manager service to fetch the transaction history
