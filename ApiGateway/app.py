f2b88bed1e873c5124961afcca149bfa3cc7e4c4
Natalija Tosic <tn190346d@student.etf.bg.ac.rs>
2024-11-12 20:07:24 +0100

    add usersuser endpoints to api gateway

--- ApiGateway/app.py
+++ ApiGateway/app.py
@@ -6,5 +6,19 @@ from werkzeug.exceptions import NotFound
 
 app = Flask(__name__, instance_relative_config=True) #instance_relative_config=True ? 
 
+user_microservice_url = "usersuser:5000"
+
+@app.route('/api/player/register', methods=['POST'])
+def register_user():
+    return request.post(f"{user_microservice_url}/api/player/register", payload=request.get_json())
+
+@app.route('/api/player/login/<int:userId>', methods=['POST'])
+def login(userId):
+    return request.post(f"{user_microservice_url}/api/player/login/{userId}")
+
+@app.route('api/player/logout/<int:userId>', method=['POST'])
+def logout(userId):
+    return request.post(f"{user_microservice_url}/api/player/logout/{userId}")
+
 def create_app():
     return app
