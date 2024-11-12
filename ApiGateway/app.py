3b5ac310ef16f9ca29774073a20eea2d18990677
JanaStevanovic <janastevanovic68@gmail.com>
2024-11-12 20:28:51 +0100

    changed api gateway admin

--- ApiGateway/app.py
+++ ApiGateway/app.py
@@ -13,23 +13,8 @@ def create_app():
 
 @app.route('/api/admin/currency/<int:user_id>', methods=['GET'])
 def get_transaction_history(user_id):
-    try:
-        # Prepare the request URL for fetching transaction history
-        transaction_history_url = CURRENCY_ADMIN_URL + f'/api/admin/currency/history/{user_id}'
+    
+        response = requests.get(CURRENCY_ADMIN_URL+f'/api/admin/currency/{user_id}')
+        return response
 
-        # Make a GET request to the database manager service to fetch the transaction history
-        response = requests.get(transaction_history_url)
-
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Return the transaction history data
-            return make_response(jsonify(response.json()), 200)
-        else:
-            # Handle errors from the database manager service
-            return make_response(jsonify({'error': 'Failed to retrieve transaction history'}), 500)
-
-    except Exception as e:
-        return make_response(jsonify({'error': str(e)}), 500)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+       
