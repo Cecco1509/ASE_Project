37848d0f1ab228fccbf5bb6d904830181e0ff2ee
Abderrahmane Salmi <abderrahmane.salmi.off@gmail.com>
2024-11-18 14:27:43 +0100

    Implemented add gacha

--- ApiGateway/app.py
+++ ApiGateway/app.py
@@ -20,6 +20,20 @@ def admin_gacha():
         return make_response(jsonify(gacha_items), 200)
     except Exception as e:
         return make_response(jsonify({"error": f"APIGateway: An unexpected error occurred: {e}"}), 400)
+    
+@app.route('/api/admin/gacha', methods=['POST'])
+def create_gacha():
+    try:
+        json_data = request.get_json()
+        if json_data:
+            response = requests.post(GACHAS_ADMIN_URL + '/api/admin/gacha', json=json_data)
+            response.raise_for_status()
+            return make_response(jsonify(response.json()), 200)
+        return make_response(jsonify({"message":"Invalid gacha data"}), 400)
+    except Exception as e:
+        return make_response(jsonify({"error": f"APIGateway: An unexpected error occurred: {e}"}), 400)
+
+# TODO: create separate files and import them here
 
 def create_app():
     return app
--- GatchasAdmin/app.py
+++ GatchasAdmin/app.py
@@ -22,5 +22,20 @@ def get_all_gacha():
     except Exception as e:
         return make_response(jsonify({"error": f"GatchaAdmin: An unexpected error occurred: {e}"}), 400)
 
+# TODO: add error handler
+# TODO: validate input
+@app.route('/api/admin/gacha', methods=['POST'])
+def create_gacha():
+    """Create a new gacha item."""
+    try:
+        json_data = request.get_json()
+        if json_data:
+            response = requests.post(DB_MANAGER_GACHA_URL + f'/gacha', json=json_data)
+            response.raise_for_status()
+            return make_response(jsonify(response.json()), 200)
+        return make_response(jsonify({"message":"Invalid gacha data"}), 400)
+    except Exception as e:
+        return make_response(jsonify({"error": f"GatchaAdmin: An unexpected error occurred: {e}"}), 400)
+
 def create_app():
     return app
