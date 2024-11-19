74b74e6bbec4b103cac2f79417fd4e39d86f57c8
Natalija Tosic <tn190346d@student.etf.bg.ac.rs>
2024-11-19 19:23:09 +0100

    fix api gateway

--- ApiGateway/app.py
+++ ApiGateway/app.py
@@ -3,22 +3,24 @@ import requests, time
 from flask import Flask, request, make_response 
 from requests.exceptions import ConnectionError, HTTPError
 from werkzeug.exceptions import NotFound
+from python_json_config import ConfigBuilder
 
 app = Flask(__name__, instance_relative_config=True) #instance_relative_config=True ? 
 
-user_microservice_url = "usersuser:5000"
+builder = ConfigBuilder()
+config = builder.parse_config('/app/config.json')
 
 @app.route('/api/player/register', methods=['POST'])
 def register_user():
-    return request.post(f"{user_microservice_url}/api/player/register", payload=request.get_json())
+    return request.post(f"{config.services.authuser}/api/player/register", payload=request.get_json())
 
 @app.route('/api/player/login/<int:userId>', methods=['POST'])
 def login(userId):
-    return request.post(f"{user_microservice_url}/api/player/login/{userId}")
+    return request.post(f"{config.services.authuser}/api/player/login/{userId}")
 
 @app.route('api/player/logout/<int:userId>', method=['POST'])
 def logout(userId):
-    return request.post(f"{user_microservice_url}/api/player/logout/{userId}")
+    return request.post(f"{config.services.authuser}/api/player/logout/{userId}")
 
 def create_app():
     return app
