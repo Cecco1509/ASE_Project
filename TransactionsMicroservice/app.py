6c37e36cba2557d8cf10afb47aa3a62c5085eb83
Lorenzo <lorenzoceccotti02@gmail.com>
2024-12-03 22:27:10 +0100

    merge admin and user transaction

--- TransactionsMicroservice/app.py
+++ TransactionsMicroservice/app.py
@@ -3,8 +3,18 @@ import requests, time
 from flask import Flask, request, make_response 
 from requests.exceptions import ConnectionError, HTTPError
 from werkzeug.exceptions import NotFound
+from handle_errors import handle_errors
+from auth_utils import *
 
 app = Flask(__name__, instance_relative_config=True) #instance_relative_config=True ? 
+
+
+@app.route('/player/', methods=['GET'])
+@handle_errors
+@validate_player_token
+def get_player_transaction_history():
+    return make_response({"message": "OK"}, 200)
+
 
 def create_app():
     return app
