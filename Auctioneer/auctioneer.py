55e5b0e5230bc6c41f1e6b975dd26f58152d4cab
Lorenzo <lorenzoceccotti02@gmail.com>
2024-12-04 18:31:34 +0100

    add authentication for auctioneer

--- Auctioneer/auctioneer.py
+++ Auctioneer/auctioneer.py
@@ -12,27 +12,44 @@ print = functools.partial(print, flush=True)
 
 builder = ConfigBuilder()
 config = builder.parse_config('/app/config.json')
-
-celery_app = Celery('worker',
-                    broker='amqp://admin:mypass@rabbit:5672', 
-                    backend='rpc://')
+credentials = builder.parse_config('/run/secrets/auctioneer_credentials')
 
 def checker():
 
-    tz = pytz.timezone('Europe/Rome')
+    ## authentication
+    auth_res = requests.post(f"{config.services.authmicroservice}/api/admin/login", json={
+        "username": credentials.username,
+        "password": credentials.password
+    }, verify=False)
+
+    if auth_res.status_code != 200:
+        print("Authentication failed")
+        return
+    
+    data = auth_res.json()
+    jwt_token = data['token']
+    headers = {
+        "Authorization": f"Bearer {jwt_token}",
+    }
+
+    body = {
+        "auctionStart" : None,
+        "auctionEnd" : None,
+        "status" : "PASSED"
+    }
 
     while True:
-        now : datetime = datetime.now(tz=tz)
+        now : datetime = datetime.now()
         print(f"Current time: {now.strftime('%d/%m/%Y, %H:%M:%S')}")
 
-        response = requests.get(f"{config.dbmanagers.auction}/auction/status/ACTIVE")
-        print("CHECKING FOR ACTIVE AUCTIONS", now)
+        response = requests.get(f"{config.dbmanagers.auction}/auction/status/ACTIVE", verify=False)
+        print("CHECKING FOR ACTIVE AUCTIONS ", now)
         if response.status_code == 200:
             auctions = response.json()
             for auction in auctions:
-                if tz.localize(datetime.strptime(auction['auctionEnd'], "%a, %d %b %Y %H:%M:%S %Z")) <= now and auction['status'] == 'ACTIVE':
-                    end_auction.delay(auction['id'])
-                    print(f"Auction {auction['id']} sendend")
+                if datetime.strptime(auction['auctionEnd'], "%a, %d %b %Y %H:%M:%S %Z") <= now and auction['status'] == 'ACTIVE':
+                    res = requests.put(f"{config.services.auction}/api/admin/auction/{auction["id"]}", headers=headers, json=body, verify=False)
+                    print(f"END AUCTION {auction["id"]} -> RESPONSE {res.status_code}")
         
         now = datetime.now()
         print(f"Sleeping for {60 - now.second} seconds")  # Sleep until next minute start.
--- AuctionsMicroservice/app.py
+++ AuctionsMicroservice/app.py
@@ -104,7 +104,6 @@ def create_auction(auth_response=None):
 @validate_player_token
 def bid_on_auction(auction_id, auth_response=None):
     try:
-        tz = pytz.timezone('Europe/Rome')
         data = request.get_json()
         response = requests.get(config.dbmanagers.auction + f'/auction/{auction_id}')
         response.raise_for_status()
@@ -113,7 +112,7 @@ def bid_on_auction(auction_id, auth_response=None):
         if (auction["status"] != "ACTIVE" ):
             return make_response(jsonify({"message": "Auction is not active"}), 400);
     
-        if (tz.localize(datetime.strptime(auction["auctionStart"], "%a, %d %b %Y %H:%M:%S %Z")).replace(second=0, microsecond=0) > datetime.now(tz).replace(second=0, microsecond=0)):
+        if (datetime.strptime(auction["auctionStart"], "%a, %d %b %Y %H:%M:%S %Z")).replace(second=0, microsecond=0) > datetime.now().replace(second=0, microsecond=0):
             return make_response(jsonify({"message": "Auction has not started yet"}), 400);
 
         if (auction["userId"] == auth_response["userId"]):
@@ -156,57 +155,59 @@ def bid_on_auction(auction_id, auth_response=None):
                 'bidCode': f"{auction_id}:{len(auction_bids)}",
                 'bidAmount': data["bidAmount"],
                 'auctionId':auction_id, 
-                'timestamp':datetime.now(tz).strftime("%Y-%m-%dT%H:%M:%SZ")
+                'timestamp':datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
             }
+        
+        try:
+            ## Subtract money from the current bidder
+            res = requests.put(f"{config.services.paymentsmicroservice}/api/player/currency/decrease/{auth_response["userId"]}",
+                        json={"amount": float(data['bidAmount'])},
+                        headers=request.headers,
+                        verify=False
+                    )
+            
+            ## This could raise an exception if the user has not enough ingameCurrency
+            res.raise_for_status()
 
+            ## Giving money back to the last bidder
+            if userBidId is not None:
+                res1 = requests.put(f"{config.services.paymentsmicroservice}/api/player/currency/increase/{userBidId}",
+                        json={"amount": float(auctionBidAmount)},
+                        headers=request.headers,
+                        verify=False
+                    )
+                
+                if res1.status_code != 200:
+                    ## Trying to rollback if the currency isn't changed
+                    res2 = requests.put(f"{config.services.paymentsmicroservice}/api/player/currency/increase/{auth_response["userId"]}",
+                        json={"amount": float(data['bidAmount'])},
+                        headers=request.headers,
+                        verify=False
+                    )
+                    res1.raise_for_status()
+            
+        except Exception as e:
+            ## Trying to rollback if the currency isn't changed
+            return make_response(jsonify({"message": "Unexpected error, retry later"}), 500)
+        
         try:
             result = requests.post(config.dbmanagers.auction + f'/auctionbid', 
                         json={
                             'userId': auth_response["userId"],
-                            'bidCode': f"{auction_id}:{len(auction_bids)}",
                             'bidAmount': data["bidAmount"],
                             'auctionId':auction_id, 
-                            'timestamp':datetime.now(tz).strftime("%Y-%m-%dT%H:%M:%SZ")
+                            'timestamp':datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
                         })
             
             result.raise_for_status()
         except Exception as e:
             print("EXPLODED "+str(e), flush=True)
             return make_response(jsonify({"message": str(e)}), 500)
-        
 
         new_bid_id = result.json()['bidId']
         bid["id"] = new_bid_id
         print("BID INSERTED", flush=True)
-        
-        try:
-            ## Subtract money from the current bidder
-            resp = requests.patch(
-                    f"{config.dbmanagers.user}/user/{auth_response["userId"]}",
-                    json={"ingameCurrency": - (float(data['bidAmount']))}
-                )
-                    
-            resp.raise_for_status()
 
-            ## Giving money back to the last bidder
-            if userBidId is not None:
-                resp1 = requests.patch(f"{config.dbmanagers.user}/user/{userBidId}",
-                                    json={"ingameCurrency": float(auctionBidAmount)}
-                                )
-                
-                if resp1.status_code != 200:
-                    ## Trying to rollback if the currency isn't changed
-                    resp2 = requests.patch(
-                        f"{config.dbmanagers.user}/user/{auth_response["userId"]}",
-                        json={"ingameCurrency": (float(data['bidAmount']))}
-                    )
-                    resp1.raise_for_status()
-            
-        except Exception as e:
-            ## Trying to rollback if the currency isn't changed
-            response = requests.delete(config.dbmanagers.auction + f'/auctionbid/{new_bid_id}')
-            print("BID REMOVED", flush=True)
-            return make_response(jsonify({"message": "Unexpected error, retry later"}), 500)
 
         return make_response(jsonify({"bid": bid}), 200)
     except Exception as err:
@@ -226,6 +227,7 @@ def auction_player_history(auth_response=None):
     except Exception as err:
         return make_response(jsonify({"message": str(err)}), 500)
 
+######################################################################################################################
 
 def check_auction(auction) -> bool:
     if auction.auctionStart > auction.auctionEnd:
@@ -236,6 +238,83 @@ def check_auction(auction) -> bool:
         return False
     
     return True
+
+
+def close_auction(auction, headers) -> bool:
+    print("Started auctioneer process", flush=True)
+
+    try:
+        auction = auction.json()
+        print(auction, flush=True)
+    except Exception as e:
+        print("Failed decode auction", flush=True)
+        return False
+
+    if not auction or auction['status'] != "ACTIVE":  # Auction already closed
+        print("Auction already closed", flush=True)
+        return False
+
+    # Close the auction
+    auction['status'] = "PASSED"
+    start = datetime.strptime(auction['auctionStart'], "%a, %d %b %Y %H:%M:%S %Z")
+    end = datetime.strptime(auction['auctionEnd'], "%a, %d %b %Y %H:%M:%S %Z")
+    timestamp = datetime.strptime(auction['timestamp'], "%a, %d %b %Y %H:%M:%S %Z")
+
+    auction['auctionStart'] = start.strftime("%Y-%m-%dT%H:%M:%SZ")
+    auction['auctionEnd'] = end.strftime("%Y-%m-%dT%H:%M:%SZ")
+    auction['timestamp'] = timestamp.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    print(auction, flush=True)
+    print("START AFTER STRF"+start.strftime("%Y-%m-%dT%H:%M:%SZ"), flush=True)
+
+    print("TRY SET TO PASSED", flush=True)
+
+    requests.put(f"{config.dbmanagers.auction}/auction/{auction["id"]}", json=auction, headers=headers, verify=False)
+
+    print("SET TO PASSED", flush=True)
+
+    # Get all bids for the auction
+    bids = requests.get(f"{config.dbmanagers.auction}/auctionbid/auction/{auction["id"]}", headers=headers, verify=False).json()
+    if not bids:
+        print("No bids for the auction", flush=True)
+        return True
+
+    # Find the winning bid
+    winningBid = max(bids, key=lambda bid: bid['bidAmount'])
+
+    # Create a transaction
+    transaction = {
+        "sellerId": auction['userId'],
+        "buyerId": winningBid['userId'],
+        "auctionBidId": winningBid['id'],
+        "timestamp": datetime.now().isoformat()
+    }
+
+
+    resp = requests.post(f"{config.dbmanagers.transaction}/auctiontransaction", headers=headers, json=transaction, verify=False)
+    if resp.status_code != 200:
+        return
+
+
+    resp = requests.put(f"{config.services.paymentsmicroservice}/api/player/currency/increase/{auction['userId']}",
+                        json={"amount": float(winningBid['bidAmount'])},
+                        headers=headers,
+                        verify=False
+                    )
+
+    # Assign Gacha to the winning bidder
+    requests.post(
+        f"{config.dbmanagers.gacha}/gachacollection",
+        json={
+            "gachaId": auction['id'],
+            "userId": winningBid['userId'],
+            "source": f"{auction['id']}"
+        }
+    )
+
+    # Remove Gacha from the seller
+    requests.delete(f"{config.dbmanagers.gacha}/gachacollection/{auction['gachaCollectionId']}")
+
 
 ##################################################### ADMIN ############################################################
 
@@ -276,26 +355,88 @@ def get_auction(auction_id, auth_response=None):
 @handle_errors
 @validate_admin_token
 def update_auction(auction_id, auth_response=None):
-    print(f"PUT auction", auction_id)
+    print(f"PUT auction ", auction_id, flush=True) 
     try:
         # Parse JSON data from the request
-        data = request.get_json()
-        if not data:
-            return make_response(jsonify({"message": "Invalid auction data"}), 400)
+        try:
+            data = request.get_json()
+            if not data:
+                return make_response(jsonify({"message": "Invalid auction data"}), 400)
+        except Exception as e:
+            return make_response(jsonify({"message": "Invalid JSON data"}), 400)
         
         # Validate auction data
-        if not check_auction(data):  # Ensure check_auction is correct
-            return make_response(jsonify({"message": "Invalid auction timestamps"}), 400)
+        if (
+            not 'auctionStart' in data or
+            not 'auctionEnd' in data or
+            not 'status' in data) :
 
-        # Send PUT request to update the auction
-        response = requests.put(
-            f"{config.dbmanagers.auction}/auction/{auction_id}",
-            json=data,
-        )
-        response.raise_for_status()  # Raise HTTPError for 4xx/5xx responses
+            return make_response(jsonify({"message": "Invalid data"}), 400);
+        
+        ## Retrieve the auction
+        res = requests.get(f"{config.dbmanagers.auction}/auction/{auction_id}", verify=False)
+        res.raise_for_status()
+
+        auction = res.json()
+        
+        if auction is None:
+            return make_response(jsonify({"message": "Auction not found"}), 404)
+        
+        if data["auctionStart"] is not None:
+            start = datetime.strptime(data['auctionStart'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        else:
+            start = None
+        
+        if data["auctionEnd"] is not None:
+            end = datetime.strptime(data['auctionEnd'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        else:
+            end = None
+        
+        now = datetime.now()
+        if end is not None:
+            end = end.replace(second=0, microsecond=0)
+        cmp_now = now.replace(second=0, microsecond=0)
+        
+        if (start is not None and auction["auctionStart"] < cmp_now):
+            return make_response(jsonify({"message": "Auction is already started, cannot modify its start time"}), 400);
+    
+        if (start is not None and start < cmp_now):
+            return make_response(jsonify({"message": "Auction start time must be in the future"}), 400);
+    
+        if end is not None and end < cmp_now:
+            return make_response(jsonify({"message": "Auction end time must be in the future"}), 400);
+    
+        if (start is not None and end is not None and start >= end):
+            return make_response(jsonify({"message": "Auction start time must be before end time"}), 400);
+    
+        if (data["status"] is not None and data["status"] == "ACTIVE"):
+            if auction["auctionEnd"] <= cmp_now:
+                return make_response(jsonify({"message": "Auction cannot be started after it has already ended"}), 400);
+
+        if data["status"] == "PASSED" and auction["status"] != "PASSED" and data["auctionEnd"] is None and data["auctionStart"] is None:
+            close_auction(auction, request.headers)
+            auction["status"] = "PASSED"
+            auction["auctionEnd"] = now
+            print("CLOSED", flush=True)
+        elif data["status"] == "PASSED" and data["auctionEnd"] is None and data["auctionStart"] is None:
+            return make_response(jsonify({"message": "Auction cannot be ended twice"}), 400);
+        elif data["status"] == "PASSED":
+            return make_response(jsonify({"message": "Cannot set start time or end time when closing an auction"}), 400);
+
+        if data["status"] != "PASSED":
+            if (data["status"] is not None):
+                auction["status"] = data["status"]
+            if (data["auctionEnd"] is not None):
+                auction["auctionEnd"] = datetime.strptime(data["auctionEnd"], "%Y-%m-%dT%H:%M:%S.%fZ")
+            if (data["auctionStart"] is not None):
+                auction["auctionStart"] = datetime.strptime(data["auctionStart"], "%Y-%m-%dT%H:%M:%S.%fZ")
+
+        # Update the auction in the database
+        res = requests.put(f"{config.dbmanagers.auction}/auction/{auction_id}", json=auction, verify=False)
+        res.raise_for_status()
         
         # Return success response
-        return make_response(jsonify({"message": "Auction updated successfully"}), 200)
+        return make_response(jsonify(auction), 200)
     except requests.exceptions.RequestException as e:
         print(f"External request failed: {e}")
         return make_response(jsonify({"message": "Failed to update auction", "error": str(e)}), 500)
