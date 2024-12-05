a731760911b8415c95abd5b9e255c2e390300c47
Lorenzo <lorenzoceccotti02@gmail.com>
2024-12-05 22:55:55 +0100

    fix error in locust file

--- locustfile.py
+++ locustfile.py
@@ -2,10 +2,10 @@ from locust import HttpUser, task, between
 
 class GachaMicroserviceTasks(HttpUser):
     """Tasks for Gacha microservice endpoints."""
-    wait_time = between(1, 2)
+    wait_time = between(1, 5)
     def on_start(self):
     # You can also do it only the first time using «if self.token:»
-        response = self.client.post("/api/player/login",json={"username": "user123", "password": "password1"}, verify=False)
+        response = self.client.post("/api/player/login",json={"username": "user1234", "password": "password1"}, verify=False)
         if response.status_code == 200:
             self.token = response.json().get("Access token")
             print("Access token obtained successfully.")
@@ -19,7 +19,7 @@ class GachaMicroserviceTasks(HttpUser):
         if self.token:
             user_id = 1  # Fiksni ID
             headers = {"Authorization": f"Bearer {self.token}"}      
-            self.client.get(f"/api/player/gacha/player-collection/{user_id}", headers=headers, verify=False)
+            self.client.get(f"/api/player/gacha/player-collection", headers=headers, verify=False)
 
     @task
     def get_collection_item_details(self):
@@ -31,10 +31,9 @@ class GachaMicroserviceTasks(HttpUser):
     @task
     def get_gacha_details(self):
         if self.token:
-            user_id = 1  # Fiksni ID
             gacha_id = 1  # Fiksni ID
             headers = {"Authorization": f"Bearer {self.token}"}
-            self.client.get(f"/api/player/gacha/player-collection/{user_id}/gacha/{gacha_id}", headers=headers, verify=False)
+            self.client.get(f"/api/player/gacha/player-collection/gacha/{gacha_id}", headers=headers, verify=False)
 
     @task
     def get_system_gacha_collection(self):
@@ -52,6 +51,6 @@ class GachaMicroserviceTasks(HttpUser):
     def roll_gacha(self):
         if self.token:
             headers = {"Authorization": f"Bearer {self.token}"}
-            self.client.post("/api/player/gacha/roll", headers=headers, verify=False)
+            self.client.post("/api/player/gacha/roll", headers=headers, verify=False, json={"rarity_level": "Common"})
 
 
